265bc1b19e0bf59065c47978f1eeefb6c1ac9fb4
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-07-11 00:57:09 +0300

    array find method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+/* @superbowlWin - searches array for years with Ws
+ * Return - year if W is found else return undefined
+**/
+
+function superbowlWin(array){
+    let result = array.find( record => record.result === "W" )
+    //syntax for ternary operator is: condition ? expressionIfTrue : expressionIfFalse
+    return result ? result.year : undefined
+}
+superbowlWin(record)
